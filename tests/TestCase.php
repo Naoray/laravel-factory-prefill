0fa951a4d9d0ff5e52f535318874c9b67a64889b
Krishan Koenig <krishan.koenig@googlemail.com>
2019-02-28 09:52:02 +0100

    fix travis test run

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -40,11 +40,12 @@ class TestCase extends Orchestra
      */
     protected function getEnvironmentSetUp($app)
     {
-        //If we're not in travis, load our local .env file
-        if (empty(getenv('CI'))) {
-            $dotenv = Dotenv::create(realpath(__DIR__ . '/..'));
-            $dotenv->load();
+        if (!file_exists(__DIR__ . '/../.env')) {
+            return;
         }
+
+        $dotenv = Dotenv::create(__DIR__ . '/..');
+        $dotenv->load();
 
         $app['config']->set('database.default', 'mysql');
         $app['config']->set('database.connections.mysql', [
