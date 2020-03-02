e6449726debb6ee7acb560002d0e2796857e0508
Krishan Koenig <krishan.koenig@googlemail.com>
2020-03-03 08:39:51 +1300

    WIP

--- tests/PrefillFactoryTest.php
+++ tests/PrefillFactoryTest.php
@@ -35,7 +35,8 @@ class PrefillFactoryTest extends TestCase
     {
         $this->loadMigrationsFrom([
             '--database' => 'mysql',
-            '--realpath' => realpath(__DIR__ . '/migrations'),
+            '--path' => realpath(__DIR__ . '/migrations'),
+            '--realpath' => true,
         ]);
     }
 
